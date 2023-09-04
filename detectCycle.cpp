82f740b3042e9d92eaad029c1a16eec2038b1662
LocNguyen <locnvt.it@gmail.com>
2023-09-04 09:45:29 +0700

    142.Linked list cycle 2

--- detectCycle.cpp
+++ detectCycle.cpp
@@ -1,7 +1,46 @@
 #include<iostream>
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+    ListNode *detectCycle(ListNode *head) {
+        if(!head) return NULL;
+
+        //initialize the two pointers
+        ListNode*fast=head;
+        ListNode*slow=head;
+
+        bool cycleFound=false;
+
+        while(fast&&fast->next)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+
+            //found the cycle
+            if(fast==slow)
+            {
+                cycleFound=true;
+                break;
+            }
+        }
+
+        if(!cycleFound) return NULL;
+
+        //move the two pointer simutaneously
+        //we have these pointer equally we will get 
+        //the starting pointer
+        while(head!=fast)
+        {
+            head=head->next;
+            fast=fast->next;
+        }
+        return head;
+    }
 int main()
 {
-    
+
 }
