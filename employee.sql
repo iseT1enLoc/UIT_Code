13688f1e72be1f49dfa515150f62820801cab932
Loc Nguyen <126844791+iseT1enLoc@users.noreply.github.com>
2024-06-24 15:20:54 +0700

    Add files via upload

---
+++ employee.sql
@@ -0,0 +1,24 @@
+﻿insert into USERS(last_name,first_name,phone_number,date_of_birth,email,pass_word,user_type) VALUES('Nguyễn','Văn A','0959953164','24/04/1935','nguyenvana854@example.com',crypt('nguyenvana',gen_salt('md5')),'employee');
+insert into USERS(last_name,first_name,phone_number,date_of_birth,email,pass_word,user_type) VALUES('Trần','Thị B','0949933127','01/04/1974','tranthib162@example.com',crypt('tranthib',gen_salt('md5')),'employee');
+insert into USERS(last_name,first_name,phone_number,date_of_birth,email,pass_word,user_type) VALUES('Lê','Văn C','0916276397','26/04/1990','levanc211@example.com',crypt('levanc',gen_salt('md5')),'employee');
+insert into USERS(last_name,first_name,phone_number,date_of_birth,email,pass_word,user_type) VALUES('Phạm','Thị D','0956031744','02/01/1945','phamthid412@example.com',crypt('phamthid',gen_salt('md5')),'employee');
+insert into USERS(last_name,first_name,phone_number,date_of_birth,email,pass_word,user_type) VALUES('Hoàng','Văn E','0936583377','15/01/1935','hoangvane935@example.com',crypt('hoangvane',gen_salt('md5')),'employee');
+insert into USERS(last_name,first_name,phone_number,date_of_birth,email,pass_word,user_type) VALUES('Vũ','Thị F','0928936370','13/01/1965','vuthif172@example.com',crypt('vuthif',gen_salt('md5')),'employee');
+INSERT INTO EMPLOYEES(user_id,onboard_date,salary) VALUES('4','26/05/2021','9000000');
+INSERT INTO EMPLOYEES(user_id,onboard_date,salary) VALUES('3','13/04/2020','15000000');
+INSERT INTO EMPLOYEES(user_id,onboard_date,salary) VALUES('1','02/09/2020','8500000');
+INSERT INTO EMPLOYEES(user_id,onboard_date,salary) VALUES('6','17/10/2021','5000000');
+INSERT INTO EMPLOYEES(user_id,onboard_date,salary) VALUES('5','04/10/2022','9500000');
+INSERT INTO EMPLOYEES(user_id,onboard_date,salary) VALUES('2','02/05/2022','6000000');
+
+insert into EMPLOYEE_ROLE(employee_id, role_id, date_assigned) values('4', '2', '26/5/2021');
+insert into EMPLOYEE_ROLE(employee_id, role_id, date_assigned) values('3', '1', '14/3/2020');
+insert into EMPLOYEE_ROLE(employee_id, role_id, date_assigned) values('1', '2', '2/9/2020');
+insert into EMPLOYEE_ROLE(employee_id, role_id, date_assigned) values('6', '3', '17/10/2021');
+insert into EMPLOYEE_ROLE(employee_id, role_id, date_assigned) values('5', '2', '04/10/2022');
+insert into EMPLOYEE_ROLE(employee_id, role_id, date_assigned) values('2', '3', '2/5/2022');
+
+
+insert into ROLES(role_id, role_name, description) values('1', 'Quản lí', 'Manager role');
+insert into ROLES(role_id, role_name, description) values('2', 'Nhân viên', 'Employee role');
+insert into ROLES(role_id, role_name, description) values('3', 'Bảo vệ', 'Security role');
---
+++ field_service.sql
@@ -0,0 +1,14 @@
+﻿-----FIELDS
+insert into FIELDS(field_id, field_name, price_per_hour) values('1', 'Sân 5',  '180000');
+insert into FIELDS(field_id, field_name, price_per_hour) values('2', 'Sân 5',  '180000');
+insert into FIELDS(field_id, field_name, price_per_hour) values('3', 'Sân 5',  '180000');
+insert into FIELDS(field_id, field_name, price_per_hour) values('4', 'Sân 5',  '180000');
+insert into FIELDS(field_id, field_name, price_per_hour) values('5', 'Sân 7',  '280000');
+insert into FIELDS(field_id, field_name, price_per_hour) values('6', 'Sân 7',  '280000');
+
+-----SERVICES
+insert into SERVICES(service_id, service_name, descriptions, price) values('1', 'Thuê áo pitch',  'cho thuê 10 áo pitch để chia đội', '20000');
+insert into SERVICES(service_id, service_name, descriptions, price) values('2', 'Thuê cone nhựa',  'cho thuê 20 cone nhựa để tập luyện chiến thuật', '15000');
+insert into SERVICES(service_id, service_name, descriptions, price) values('3', 'Thuê khung thành mini ',  'cho thuê 3 khung thành mini để tập luyện chiến thuật', '30000');
+insert into SERVICES(service_id, service_name, descriptions, price) values('4', 'Thuê giày',  'cho thuê giày đá bóng', '25000');
+insert into SERVICES(service_id, service_name, descriptions, price) values('5', 'Thuê găng thủ môn',  'cho thuê 2 găng tay giành cho thủ môn', '15000');
